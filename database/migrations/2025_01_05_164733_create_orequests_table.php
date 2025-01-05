f9aab931260ca58d5cf7139c47a42938b04eb463
bahareh <baharehviani@gmail.com>
2025-01-05 20:22:15 +0330

    update

---
+++ database/migrations/2025_01_05_164733_create_orequests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orequests', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('user_id')->constrained();
+            $table->text('type');
+            $table->text('description')->nullable();
+            $table->text('pickup_location');
+            $table->text('dropoff_location');
+            $table->text('weight');
+            $table->text('cost');
+            $table->text('status', ['pending','accepted','declined'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orequests');
+    }
+};
---
+++ database/migrations/2025_01_05_164830_create_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('user_id')->constrained();
+            $table->text('type');
+            $table->text('plate_number');
+            $table->text('description')->nullable();
+            $table->text('maximum_capacity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
