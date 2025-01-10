098abd2d453d15bd533433e4519380255e78ef95
bahareh <baharehviani@gmail.com>
2025-01-10 20:01:35 +0330

    fixes

--- app/Http/Controllers/CourierControllers/OrderRequestsController.php
+++ app/Http/Controllers/CourierControllers/OrderRequestsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\CourierControllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\OrderRequest;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class OrderRequestsController extends Controller
 {
