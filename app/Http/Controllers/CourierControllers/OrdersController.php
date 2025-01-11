61c269c8132e0556c36a62aa67091aa870407e74
bahareh <baharehviani@gmail.com>
2025-01-11 22:10:40 +0330

    sth

--- app/Http/Controllers/CourierControllers/OrdersController.php
+++ app/Http/Controllers/CourierControllers/OrdersController.php
@@ -62,7 +62,10 @@ class OrdersController extends Controller
 
         return[
             'status' => 'SUCCESSFUL',
-            'message' => 'REQUEST_ACCEPTED_AND_ORDER_CREATED_SUCCESSFULY'
+            'message' => 'REQUEST_ACCEPTED_AND_ORDER_CREATED_SUCCESSFULY',
+            'payload' => [
+                'order' => $new_order
+            ]
         ];
     }
 
