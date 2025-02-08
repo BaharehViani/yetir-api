8a4eaea1813b32da273f1bc269bbd772a25c5b88
bahareh <baharehviani@gmail.com>
2025-02-08 17:10:39 +0330

    invoice orderby

--- app/Http/Controllers/CustomerControllers/InvoicesController.php
+++ app/Http/Controllers/CustomerControllers/InvoicesController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 class InvoicesController extends Controller
 {
     public function indexAll(Request $request) {
-        return Invoice::where('user_id', $request->user()->id)->orderBy('updated_at', 'asc')->get();
+        return Invoice::where('user_id', $request->user()->id)->orderBy('status', 'desc')->get();
     }
 
     public function indexPending(Request $request) {
